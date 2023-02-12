2cefabbf22a4efc7131833125aff3c41bc8aeb38
denis <batalov@jivosite.com>
2023-02-12 22:55:13 +0500

    Dropdown control

--- src/core/components/Select/index.tsx
+++ src/core/components/Select/index.tsx
@@ -1,5 +1,5 @@
 import {h, FunctionalComponent} from 'preact';
-import {useMemo, useState, useCallback, useEffect} from 'preact/compat';
+import {useMemo, useState, useCallback, useEffect, useRef} from 'preact/compat';
 import style from './style.less';
 import cn from 'classnames';
 
@@ -9,33 +9,51 @@ interface IProps {
   onChange: (key: string) => void;
 }
 
+const MAX_HEIGHT_OPTIONS_LIST = 200;
+
 export const Select: FunctionalComponent<IProps> = ({value, options, onChange}) => {
   const [isActive, setIsActive] = useState(false);
+  const [position, setPosition] = useState('top');
+  const buttonRef = useRef<HTMLDivElement>();
+  const screen = window.parent || window;
+
   const onOutClick = useCallback(() => {
     setIsActive(false);
   }, []);
 
   useEffect(() => {
-    return () => document.removeEventListener('click', onOutClick);
+    return () => screen.removeEventListener('click', onOutClick);
   }, []);
 
   useEffect(() => {
     isActive
-      ? document.addEventListener('click', onOutClick)
-      : document.removeEventListener('click', onOutClick);
+      ? screen.addEventListener('click', onOutClick)
+      : screen.removeEventListener('click', onOutClick);
   }, [isActive]);
 
   const selectedItem = useMemo(() => {
     return options.find(item => item.key === value)?.value ?? 'Not selected';
   }, [value, options]);
 
+  const onClick = () => {
+    if (!isActive && buttonRef.current) {
+      const rect = buttonRef.current.getBoundingClientRect();
+
+      const freeSpace = screen.innerHeight - (rect?.top + rect?.height);
+
+      setPosition(freeSpace < MAX_HEIGHT_OPTIONS_LIST ? 'bottom' : 'top');
+    }
+
+    setIsActive(!isActive);
+  };
+
   return (
-    <tdiv className={style.control}>
-      <tdiv className={style.button} onClick={() => setIsActive(!isActive)}>
+    <tdiv className={style.control} ref={buttonRef}>
+      <tdiv className={style.button} onClick={onClick}>
         {selectedItem}
       </tdiv>
       {isActive ? (
-        <tdiv className={style.options}>
+        <tdiv className={cn(style.options, style[position])}>
           <tdiv className={cn(style.list, 'scroll')}>
             {options?.map(item => (
               <tdiv key={item.key} onClick={() => onChange(item.key)}>
